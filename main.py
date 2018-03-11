2d487559f7a93aa31481ecd605a42abc7d340e82
Mark Goldwater <mark.goldwater@students.olin.edu>
2018-03-11 16:21:29 -0400

    Scratched last zoom animation
    A click on a circle now leads to the disappearence of all "home circles" and the appearence of that movies corresponding "sub circles"

--- main.py
+++ main.py
@@ -3,6 +3,10 @@
 import pygame
 from pygame.locals import *
 import time
+import math
+
+color = 100,149,237
+screen_color = 255,250,240
 
 class PyGameWindowView(object):
     """ A view of movie data visualizer rendered in a Pygame window """
@@ -12,6 +16,7 @@ class PyGameWindowView(object):
             containing the width and height """
         self.model = model
         self.screen = pygame.display.set_mode(size)
+        self.home = True
 
     def draw(self):
         """ Draw the current game state to the screen """
@@ -23,12 +28,24 @@ class PyGameWindowView(object):
                              dot.radius)
         pygame.display.update()
 
+    def zoom(self, dot):
+        """Displays the individual movie-rating dots for the inputed 'dot'"""
+        self.screen.fill(pygame.Color(255,250,240))
+        self.model.dots = self.model.dot_to_child[dot.label]
+
+
 class VisualizerModel(object):
     """ Encodes a model of the game state """
     def __init__(self, size):
         self.size = size
         self.dots = []
-        self.dots.append(Dot(50, size[0]//2, size[1]//2))
+        self.home_dots = []
+        self.dot_to_child = {}
+        self.dots.append(Dot(50, 80, 80, "test"))
+
+        for dot in self.dots:
+            self.dot_to_child[dot.label] = [Dot(50, 160, 160, "fandango"), Dot(50, 240, 240, "rt")]
+            self.home_dots.append(dot)
 
     def __str__(self):
         output_lines = []
@@ -40,42 +57,40 @@ class VisualizerModel(object):
 
 class Dot(object):
     """ Encodes the state of a dot in the visualizer """
-    def __init__(self, radius, x, y):
+    def __init__(self, radius, x, y, label=None):
+        if label == None:
+            label = ""
+
+        self.label = label
         self.radius = radius
         self.x = x
         self.y = y
-        self.color = 100,149,237
+        self.color = color
 
     def __str__(self):
         return "Dot radius=%f, x=%f, y=%f" % (self.radius, self.x, self.y)
 
-    def zoomStep(self):
-        # Increases size of circle when clicked on
-        self.radius += 3
-        time.sleep(0.0001)
 
 class PyGameMouseController(object):
-    """ A controller that uses the mouse to move the paddle """
+    """ Uses the mouse to zoom in and out of a specific movie. """
     def __init__(self,model,view):
         self.model = model
         self.view = view
 
     def handle_event(self,event):
-        """ Handle the mouse event so the paddle tracks the mouse position """
-        if event.type == MOUSEBUTTONUP:
-            dot = self.model.dots[0]
+        """ Handle the mouse event so that when a movie is clicked on, its individual movie-rating sites are displayed.
+            of those are already displayed, then it will go back to the movie dots."""
 
-            originalRadius = dot.radius
-            originalColor = dot.color
-            for _ in range(400):
-                dot.zoomStep()
-                view.draw()
+        if event.type == MOUSEBUTTONUP and self.view.home:
+            for dot in self.model.dots:
+                #checks which circle the mouse click was in the radius of
+                if math.hypot(dot.x - pygame.mouse.get_pos()[0], dot.y - pygame.mouse.get_pos()[1]) < dot.radius:
+                    self.view.zoom(dot)
+                    self.view.home = not self.view.home
 
-            dot.color = (255,255,255)
-            dot.radius = originalRadius
-            dot.color = originalColor
-
-
+        elif event.type == MOUSEBUTTONUP and not self.view.home:
+            self.model.dots = self.model.home_dots
+            self.view.home = not self.view.home
 
 
 if __name__ == '__main__':
@@ -84,7 +99,7 @@ if __name__ == '__main__':
     size = (640, 480)
 
     model = VisualizerModel(size)
-    print(model)
+    print("Home dots:", model)
     view = PyGameWindowView(model, size)
 
     controller = PyGameMouseController(model,view)
@@ -95,5 +110,5 @@ if __name__ == '__main__':
             if event.type == QUIT:
                 running = False
             controller.handle_event(event)
-        #model.update()
         view.draw()
+    pygame.quit()
